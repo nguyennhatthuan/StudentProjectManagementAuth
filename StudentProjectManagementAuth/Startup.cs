97bf62d9e5a5be10f050a0cb0ff98ef7cf1da373
Thuan Nguyen <nhatthuandiend@gmail.com>
2017-04-06 21:41:40 +0700

    Add project files.

---
+++ StudentProjectManagementAuth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StudentProjectManagementAuth.Startup))]
+namespace StudentProjectManagementAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
