15c1d22feaf380d7af215de25e61adbdf2a4a2f5
Thuan Nguyen <nhatthuandiend@gmail.com>
2017-04-06 23:35:10 +0700

    [Areas] Add 3 Areas and Make Manage Role & User

---
+++ StudentProjectManagementAuth/Areas/Administrator/Controllers/ManageRoleController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using StudentProjectManagementAuth.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentProjectManagementAuth.Areas.Administrator.Controllers
+{
+    public class ManageRoleController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Administrator/ManageRole
+        public ActionResult Index()
+        {
+            var model = db.Roles.AsEnumerable();
+            return View(model);
+        }
+
+        public ViewResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IdentityRole role)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Roles.Add(role);
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+            }
+            return View(role);
+        }
+
+        public ActionResult Delete(string Id)
+        {
+            var model = db.Roles.Find(Id);
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(string Id)
+        {
+            IdentityRole model = null;
+            try
+            {
+                model = db.Roles.Find(Id);
+                db.Roles.Remove(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+            }
+            return View(model);
+        }
+    }
+}
---
+++ StudentProjectManagementAuth/Areas/Administrator/Controllers/ManageUserController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity.EntityFramework;
+using StudentProjectManagementAuth.Models;
+
+namespace Demo_ASP.NET_Identity.Areas.Admin.Controllers
+{
+    [Authorize(Users = "nhatthuandiend@gmail.com")]
+    public class ManageUserController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Admin/ManageUser
+        public ActionResult Index()
+        {
+            IEnumerable<ApplicationUser> model = db.Users.AsEnumerable();
+            return View(model);
+        }
+
+        public ActionResult Edit(string Id)
+        {
+            ApplicationUser model = db.Users.Find(Id);
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ApplicationUser model)
+        {
+            try
+            {
+                db.Entry(model).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return View(model);
+            }
+        }
+
+        public ActionResult EditRole(string Id)
+        {
+            ApplicationUser model = db.Users.Find(Id);
+            ViewBag.RoleId = new SelectList(db.Roles.ToList().Where(item => model.Roles.FirstOrDefault(r => r.RoleId == item.Id) == null).ToList(), "Id", "Name");
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddToRole(string UserId, string[] RoleId)
+        {
+            ApplicationUser model = db.Users.Find(UserId);
+            if (RoleId != null && RoleId.Count() > 0)
+            {
+                foreach (string item in RoleId)
+                {
+                    IdentityRole role = db.Roles.Find(RoleId);
+                    model.Roles.Add(new IdentityUserRole() { UserId = UserId, RoleId = item });
+                }
+                db.SaveChanges();
+            }
+            ViewBag.RoleId = new SelectList(db.Roles.ToList().Where(item => model.Roles.FirstOrDefault(r => r.RoleId == item.Id) == null).ToList(), "Id", "Name");
+            return RedirectToAction("EditRole", new { Id = UserId });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteRoleFromUser(string UserId, string RoleId)
+        {
+            ApplicationUser model = db.Users.Find(UserId);
+            model.Roles.Remove(model.Roles.Single(m => m.RoleId == RoleId));
+            db.SaveChanges();
+            ViewBag.RoleId = new SelectList(db.Roles.ToList().Where(item => model.Roles.FirstOrDefault(r => r.RoleId == item.Id) == null).ToList(), "Id", "Name");
+            return RedirectToAction("EditRole", new { Id = UserId });
+        }
+
+        public ActionResult Delete(string Id)
+        {
+            var model = db.Users.Find(Id);
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(string Id)
+        {
+            ApplicationUser model = null;
+            try
+            {
+                model = db.Users.Find(Id);
+                db.Users.Remove(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return View("Delete", model);
+            }
+        }
+    }
+}
