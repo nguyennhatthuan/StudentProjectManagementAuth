e2de36f1e94a15d033db3d7eea72aad5ad5c514b
Thuan Nguyen <nhatthuandiend@gmail.com>
2017-04-11 22:08:51 +0700

    [Models] Update Models

---
+++ StudentProjectManagementAuth/Models/ProjectModels/ProjectDetails.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace StudentProjectManagementAuth.Models
+{
+    public class ProjectDetails
+    {
+        [Key]
+        public string ProjectId { get; set; }
+        [Key]
+        public string GroupId { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+
+        [ForeignKey("ProjectId")]
+        public virtual Project Project { get; set; }
+        [ForeignKey("GroupId")]
+        public virtual Group Group { get; set; }
+    }
+}
